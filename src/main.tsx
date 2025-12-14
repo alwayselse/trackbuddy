091ee4a72fcebc761134cdf26bf72ec014b73294
alwayselse <nikhilvatsya@gmail.com>
2025-12-14 19:36:44 +0530

    Fix routing for GitHub Pages subdirectory

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/trackbuddy">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
